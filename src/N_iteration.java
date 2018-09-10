599989ac9489900b34e16ce64cbbb30402c6664b
aishwa8141 <a.aish1997@gmail.com>
2018-09-10 16:51:02 +0530

    second commit

--- src/N_iteration.java
+++ src/N_iteration.java
@@ -6,7 +6,7 @@ public class N_iteration {
 
         Scanner scan=new Scanner(System.in);
 
-        System.out.println("enter a number");
+        System.out.println("enter a number n for nth iteration");
         int n=scan.nextInt();
 
         for (int i=1;i<=n;i++)
--- src/integersum.java
+++ src/integersum.java
@@ -5,7 +5,7 @@ public class integersum {
     public static void main(String [] args){
         Scanner n=new Scanner(System.in);
         int total=0;
-        System.out.println("input");
+        System.out.println("input: enter 0 to stop");
         int num=n.nextInt();
          while (num!=0){
              total += num;
